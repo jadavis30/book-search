7cccf06f893d642b404800e716874729a00c21a8
Jared Davis <jared.davis21@yahoo.com>
2021-04-04 12:04:23 -0400

    typeDef updated for query and mutation

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,8 +3,47 @@ const { gql } = require('apollo-server-express');
 
 // create our typeDefs
 const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount:Int
+        savedBooks: [Book]
+    }
+
+    type Book {
+        bookId: String!
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+
+    type Auth {
+        token:ID!
+        user: User
+    }
+
     type Query {
-        helloWorld:String
+        users: [User]
+        me: User
+    }
+
+    input BookInput {
+        authors: [String]
+        description: String
+        title: String
+        bookId: String!
+        image: String
+        link: String
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(book: BookInput!): User
+        removeBook(bookId: String!): User
     }
 `;
 
