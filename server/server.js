64eeded3bccde5753a8e24453a2b461d2875f0ba
Jared Davis <jared.davis21@yahoo.com>
2021-04-04 12:30:01 -0400

    server typo corrected and updated with authMiddleware

--- server/server.js
+++ server/server.js
@@ -1,18 +1,17 @@
 const express = require('express');
 const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
 const { ApolloServer } = require('apollo-server-express');
 
 const { typeDefs, resolvers } = require('../server/schemas');
 const db = require('./config/connection');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
 const server = new ApolloServer({
   typeDefs,
-  resovlers
+  resolvers,
+  context: authMiddleware
 });
 
 server.applyMiddleware({ app });
@@ -25,8 +24,13 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`)
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
