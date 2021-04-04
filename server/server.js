56f7635e08d1db4c0908f21e61915a08c14c59e7
Jared Davis <jared.davis21@yahoo.com>
2021-04-04 11:51:10 -0400

    gitignore updated to https://github.com/github/gitignore/blob/master/Node.gitignore

--- server/server.js
+++ server/server.js
@@ -2,11 +2,22 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+const { ApolloServer } = require('apollo-server-express');
+
+const { typeDefs, resolvers } = require('../server/schemas');
+const db = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
+const server = new ApolloServer({
+  typeDefs,
+  resovlers
+});
+
+server.applyMiddleware({ app });
+
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
